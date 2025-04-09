4c06269901f7d6ff2d14275b9b88afcf3fe59154
Beenz <CH.SABEEN@GMAIL.COM>
2025-04-09 11:55:32 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import EmojiList from './Main.js';
+//import MovieList from './Movies';
+import EmojiCategories from './EmojiCategories.js';
+import MovieList from './FetchMovieTest';
+
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+       <Header/>
+     </div>
+     <div>
+       <EmojiList />
+        {/* <EmojiCategories/>  */}
+         {/* <MovieList/>   */}
+       
+       
+    </div>
+    </>
+  );
+}
+
+export default App;
+
