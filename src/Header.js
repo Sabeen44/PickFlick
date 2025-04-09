a0047321a734552a214d9ab35bae366a9852bcf6
Beenz <CH.SABEEN@GMAIL.COM>
2025-04-09 12:12:34 -0700

    Mood Array and Header added

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+ 
+ 
+ const Header = () => {
+      return (
+          <header className="bg-primary text-white text-center py-3 custom-header"> <h1>My Mood Movie</h1> </header> 
+         ); 
+     }; 
+     
+     export default Header;
